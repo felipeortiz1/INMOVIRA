80a9592326ebd72740e32f65ab38e0b02219ecb3
Andrey Medina <bankrey08@gmail.com>
2025-10-27 11:58:03 -0500

    Modificacion de modelo

--- app/Models/Imagen.php
+++ app/Models/Imagen.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Imagen extends Model
 {
-    //
+    protected $table = 'imagenes';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'url_imagen',
+        'id_inmueble'
+    ];
+
+    public function inmueble()
+    {
+        return $this->belongsTo(Inmueble::class, 'id_inmueble');
+    }
 }
--- app/Models/Inmueble.php
+++ app/Models/Inmueble.php
@@ -6,5 +6,48 @@ use Illuminate\Database\Eloquent\Model;
 
 class Inmueble extends Model
 {
-    //
+
+    protected $table = 'inmuebles';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'direccion',
+        'titulo',
+        'tipoOferta',
+        'precio',
+        'precioAdministracion',
+        'area',
+        'n_habitaciones',
+        'n_baños',
+        'n_parqueaderos',
+        'n_piso',
+        'pisoNumero',
+        'descripcion',
+        'fechaPublicacion',
+        'estadoPublicacion',
+        'fechaCreacion',
+        'id_usuario',
+        'id_barrio',
+        'id_tipo'
+    ];
+
+    // Relaciones
+    public function usuario()
+    {
+        return $this->belongsTo(Usuario::class, 'id_usuario');
+    }
+
+    public function barrio()
+    {
+        return $this->belongsTo(Barrio::class, 'id_barrio');
+    }
+
+    public function tipoInmueble()
+    {
+        return $this->belongsTo(TipoInmueble::class, 'id_tipo');
+    }
+
+    public function imagenes()
+    {
+        return $this->hasMany(Imagen::class, 'id_inmueble');
+    }
 }

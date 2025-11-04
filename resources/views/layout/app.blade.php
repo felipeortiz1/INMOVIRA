871d29e574bab31c51b3b386fee739f9761f68bf
Andrey Medina <bankrey08@gmail.com>
2025-11-04 11:06:12 -0500

    se agregó script para alertas de eliminacion de datos asociadas

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -3,25 +3,42 @@
 @section('titulo', $title ?? 'Dashboard')
 
 @section('content_header')
-    <h1>@yield('page-title', 'Panel de Administrador')</h1>
+<h1>@yield('page-title', 'Panel de Administrador')</h1>
 @stop
 
 @section('contenido')
-    <div class="container mt-4">
-        <div class="row">
-            @yield('content')
-        </div>
+<div class="container mt-4">
+    <div class="row">
+        @yield('content')
     </div>
+</div>
 @stop
 
 @section('css')
-    <link rel="stylesheet" href="{{ asset('css/admin-custom.css') }}">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">    @stack('styles')
+<link rel="stylesheet" href="{{ asset('css/admin-custom.css') }}">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"> @stack('styles')
 @stop
 
 @section('js')
-    @stack('scripts')
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+@stack('scripts')
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+@if(session('error'))
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        $(function() {
+            $(document).Toasts('create', {
+                class: 'bg-danger',
+                title: 'Error',
+                body: "{{ session('error') }}",
+                autohide: true,
+                delay: 5000
+            }).show();
+        });
+    });
+</script>
+@endif
+
 @stop

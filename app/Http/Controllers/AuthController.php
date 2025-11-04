08c720554dcf4faf26846089939e95efc2c1972e
felipeortiz1 <za09099az@gmail.com>
2025-11-04 11:59:21 -0500

    Se creo el login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    //
+
+    public function verlogin(Request $request)
+    {
+        if (Auth::check()) {
+            return redirect()->route('welcome');
+        }
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->route('welcome')
+                ->with('success', 'Has iniciado sesión correctamente.');
+        }
+
+        return back()->withErrors([
+            'email' => 'Las credenciales proporcionadas no son correctas.',
+        ])->onlyInput('email');
+    }
+
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect()->route('login')
+            ->with('success', 'Has cerrado sesión correctamente.');
+    }
+
+    public function verRegistro()
+    {
+        if (Auth::check()) {
+            return redirect()->route('dash.index');
+        }
+        return view('auth.register');
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:100',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        // Crear el usuario
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        // Autenticamos automáticamente al usuario
+        Auth::login($user);
+
+        return redirect()->route('dash.index')->with('success', 'Registro exitoso, bienvenido a Supertiendas');
+    }
+}

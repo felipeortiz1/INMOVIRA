bdbba8101e369fa790111b704c165ef5f8f63856
Andrey Medina <bankrey08@gmail.com>
2025-11-06 07:45:37 -0500

    Se creo plantilla de modal

---
+++ resources/views/Inmuebles/partials/show-modal.blade.php
@@ -0,0 +1,18 @@
+<div class="modal fade" id="showInmuebleModal" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
+        <div class="modal-content">
+            <div class="modal-header bg-primary text-white">
+                <h5 class="modal-title">Detalle del inmueble</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+            </div>
+            <div class="modal-body">
+                <div id="modal-inmueble-contenido">
+                    <div class="text-center text-muted py-3">Cargando...</div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+            </div>
+        </div>
+    </div>
+</div>
